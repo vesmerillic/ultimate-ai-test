0d1172278bdcbe60d24df00e36b6e097f9db4df5
vesmerillic <bojar.damian@gmail.com>
2019-07-25 18:21:00 +0200

    Added multi-instance test to suite, fix formatting

--- __tests__/ConversationEngine.spec.ts
+++ __tests__/ConversationEngine.spec.ts
@@ -1,78 +1,87 @@
-import { ConversationEngine } from '../src/engine/ConversationEngine';
-import * as troubleshootingJSON from "../src/inputs/troubleshooting.json";
+import { ConversationEngine } from '../src/engine/ConversationEngine'
+import * as troubleshootingJSON from "../src/inputs/troubleshooting.json"
 
 describe('ConversationEngine tests', () => {
 
   it('Simple conversations', () => {
-    let conversation = new ConversationEngine(troubleshootingJSON);
+    let conversation = new ConversationEngine(troubleshootingJSON)
     testSimpleConversation(conversation)
     testSimpleConversation(conversation)
     testSimpleConversation(conversation)
-  });
+  })
 
   it('Incorrect starts of conversation, cold start', () => {
-    let conversation = new ConversationEngine(troubleshootingJSON);
+    let conversation = new ConversationEngine(troubleshootingJSON)
     testIncorrectStarts(conversation)
-  });
+  })
 
   it('Incorrect starts of conversation, after conversation', () => {
-    let conversation = new ConversationEngine(troubleshootingJSON);
+    let conversation = new ConversationEngine(troubleshootingJSON)
     testSimpleConversation(conversation)
     testIncorrectStarts(conversation)
-  });
+  })
 
   it('Incorrect answer: retry', () => {
-    let conversation = new ConversationEngine(troubleshootingJSON);
+    let conversation = new ConversationEngine(troubleshootingJSON)
     testUnfinishedConversation(conversation)
-    expect(conversation.reply('Hello')).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT);
-    expect(conversation.reply(null)).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT);
-    expect(conversation.reply(undefined)).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT);
+    expect(conversation.reply('Hello')).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT)
+    expect(conversation.reply(null)).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT)
+    expect(conversation.reply(undefined)).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT)
     //test answer from one of the previous questions
-    expect(conversation.reply('My internet doesn\'t work')).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT);
+    expect(conversation.reply('My internet doesn\'t work')).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT)
     //continue the conversation
-    expect(conversation.reply('Yes')).toBe(`Contact our customer support for more help.`);
-  });
+    expect(conversation.reply('Yes')).toBe(`Contact our customer support for more help.`)
+  })
 
   it('Incorrect answer: going back a step', () => {
-    let conversation = new ConversationEngine(troubleshootingJSON);
+    let conversation = new ConversationEngine(troubleshootingJSON)
     testUnfinishedConversation(conversation)
-    expect(conversation.reply('Hello')).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT);
-    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_LAST_STEP)).toBe('Have you tried resetting your router?');
-    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_LAST_STEP)).toBe('What kind of problem are you facing?');
+    expect(conversation.reply('Hello')).toBe(ConversationEngine.INCORRECT_ANSWER_INPUT)
+    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_LAST_STEP)).toBe('Have you tried resetting your router?')
+    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_LAST_STEP)).toBe('What kind of problem are you facing?')
     //trying to go up to non existing steps
-    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_LAST_STEP)).toBe('What kind of problem are you facing?');
-    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_LAST_STEP)).toBe('What kind of problem are you facing?');
-  });
+    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_LAST_STEP)).toBe('What kind of problem are you facing?')
+    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_LAST_STEP)).toBe('What kind of problem are you facing?')
+  })
 
   it('Incorrect answer: restart conversation', () => {
-    let conversation = new ConversationEngine(troubleshootingJSON);
+    let conversation = new ConversationEngine(troubleshootingJSON)
     testUnfinishedConversation(conversation)
-    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_RESTART)).toBe('What kind of problem are you facing?');
+    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_RESTART)).toBe('What kind of problem are you facing?')
     //restarting multiple times
-    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_RESTART)).toBe('What kind of problem are you facing?');
-    expect(conversation.reply('My phone doesn\'t work')).toBe(`What is the make and model of your phone?`);
-    expect(conversation.reply('iPhone X')).toBe(`Contact Apple service?`);
-  });
+    expect(conversation.reply(ConversationEngine.RECOVERY_ANSWER_RESTART)).toBe('What kind of problem are you facing?')
+    expect(conversation.reply('My phone doesn\'t work')).toBe(`What is the make and model of your phone?`)
+    expect(conversation.reply('iPhone X')).toBe(`Contact Apple service?`)
+  })
 
-});
+  it('Test multi-instance', () => {
+    let conversation1 = new ConversationEngine(troubleshootingJSON)
+    let conversation2 = new ConversationEngine(troubleshootingJSON)
+    testUnfinishedConversation(conversation1)
+    testUnfinishedConversation(conversation2)
+    expect(conversation1.reply('Yes')).toBe(`Contact our customer support for more help.`)
+    expect(conversation2.reply('Yes')).toBe(`Contact our customer support for more help.`)
+  })
+
+})
 
 function testIncorrectStarts(conversation: ConversationEngine){
-  expect(conversation.reply('Hello')).toBe(ConversationEngine.INCORRECT_START_INPUT);
-  expect(conversation.reply(null)).toBe(ConversationEngine.INCORRECT_START_INPUT);
-  expect(conversation.reply(undefined)).toBe(ConversationEngine.INCORRECT_START_INPUT);
-  expect(conversation.reply('null')).toBe(ConversationEngine.INCORRECT_START_INPUT);
+  expect(conversation.reply('Hello')).toBe(ConversationEngine.INCORRECT_START_INPUT)
+  expect(conversation.reply(null)).toBe(ConversationEngine.INCORRECT_START_INPUT)
+  expect(conversation.reply(undefined)).toBe(ConversationEngine.INCORRECT_START_INPUT)
+  expect(conversation.reply('null')).toBe(ConversationEngine.INCORRECT_START_INPUT)
   //actually start the conversation
-  expect(conversation.reply('')).toBe(`What kind of problem are you facing?`);
+  expect(conversation.reply('')).toBe(`What kind of problem are you facing?`)
 }
 
 function testSimpleConversation(conversation: ConversationEngine){
-  expect(conversation.reply('')).toBe(`What kind of problem are you facing?`);
-  expect(conversation.reply('My phone doesn\'t work')).toBe(`What is the make and model of your phone?`);
-  expect(conversation.reply('Samsung Galaxy S10')).toBe(`Contact Samsung service?`);
+  expect(conversation.reply('')).toBe(`What kind of problem are you facing?`)
+  expect(conversation.reply('My phone doesn\'t work')).toBe(`What is the make and model of your phone?`)
+  expect(conversation.reply('Samsung Galaxy S10')).toBe(`Contact Samsung service?`)
 }
 
 function testUnfinishedConversation(conversation: ConversationEngine){
-  expect(conversation.reply('')).toBe(`What kind of problem are you facing?`);
-  expect(conversation.reply('My internet doesn\'t work')).toBe(`Have you tried resetting your router?`);
-  expect(conversation.reply('Yes')).toBe(`Have you tried with another cable?`);
+  expect(conversation.reply('')).toBe(`What kind of problem are you facing?`)
+  expect(conversation.reply('My internet doesn\'t work')).toBe(`Have you tried resetting your router?`)
+  expect(conversation.reply('Yes')).toBe(`Have you tried with another cable?`)
 }
