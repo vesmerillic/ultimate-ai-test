d32d01d65820b8b27718dca873799f4fa01f5e28
bojar.damian@gmail.com <bojar.damian@gmail.com>
2023-05-18 21:31:11 +0200

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwMTU4OA/spectral.js?branch_slug=master&token=e9dcd313-b7b1-4ac5-a6d1-e2fbcde29162";
+export default { extends: ruleset };
---
+++ docs/Our-API-tutorial-LOL-‎😃.md
@@ -0,0 +1,9 @@
+---
+stoplight-id: xn18hy1d9k4gn
+---
+
+# Our-API-tutorial-LOL-‎😃
+
+The beginning of an awesome article...
+
+Hello this is a new article lol we write stuff here
